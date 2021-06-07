3445716eb84ae9e4e9afba5952f15b135b0fbc34
Sam Carbine <scarbine@me.com>
2021-06-07 11:13:04 -0500

    Added on click to display orders created

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -5,21 +5,18 @@ const buildOrderListItem = (order) => {
         Order #${order.id} was placed on ${order.timestamp}
     </li>`
 }
-
 export const Orders = () => {
     /*
         Can you explain why the state variable has to be inside
         the component function for Orders, but not the others?
     */
     const orders = getOrders()
-
+    
     let html = "<ul>"
-
     const listItems = orders.map(buildOrderListItem)
-
     html += listItems.join("")
     html += "</ul>"
-
     return html
 }
+
 
